c216e6eed8cd754b8f29c3cbd7aafd365590b350
Mike Lukina <mikelukina@gmail.com>
2019-01-28 13:54:47 -0500

    Added dynamic titles for HTML layout page and a second HTML page to test

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
+
+@app.route('/test')
+def test():
+    return render_template('test.html', title='Test')
 
 if __name__ == '__main__':
     app.run(debug=True)
