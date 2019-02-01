f88b3017469e498eaeb16b9b436e801952b3e5c9
Mike Lukina <mikelukina@gmail.com>
2019-01-31 23:43:19 -0500

    Installed latest version of pip and recreated the venv folder with Flask

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=c:\apps\health-screener\venv"
+set "VIRTUAL_ENV=C:\projects\health-screener\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
