26bfbf3545111a269a75f555f11bfeadba54dfcf
Mike Lukina <mikelukina@gmail.com>
2019-03-25 00:40:00 -0400

    Bootstrap intro, seperation of screener sections, styling changes

--- app.py
+++ app.py
@@ -37,6 +37,37 @@ def index():
     return render_template('index.html', title='Home', screener_instance=
         screener_instance)
 
+@app.route('/team4/dashboard')
+def dashboard():
+    # Needs to be modified at some point to only return instances recorded by the assessor
+    if request.method == "GET":
+        cursor = g.db_conn.cursor()
+        cursor.execute('SELECT id, first_name, last_name, time_recorded FROM screener_instance ORDER BY time_recorded DESC LIMIT 5')
+        screener_instance = [dict(id=row[0], first_name=row[1], last_name=
+            row[2], time_recorded=row[3]) for row in cursor.fetchall()]
+        cursor.close()
+
+    return render_template('dashboard.html', title='Home', screener_instance=
+        screener_instance)       
+
+@app.route('/team4/identification_information', methods=['GET', 'POST'])
+def identification_information():
+    if request.method == 'GET':
+        return render_template('identification_information.html')
+    
+    elif request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+
+        cursor = g.db_conn.cursor()
+        cursor.execute('INSERT INTO screener_instance(first_name, last_name) VALUES (%s, %s)', 
+            (first_name, last_name))
+
+        g.db_conn.commit()
+        cursor.close()
+
+        return redirect(url_for('screener_instance'))
+
 @app.route('/team4/screener_instance', methods=['GET', 'POST'])
 def screener_instance():
     if request.method == 'GET':
